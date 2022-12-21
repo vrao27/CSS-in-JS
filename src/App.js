b34ba07c87640c5b24cf0b276b57c0f25f1e023b
Vinay Rao <vrao8912@gmail.com>
2022-12-21 15:08:39 +0100

    buttons styled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import logo from "./logo.png";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 // import `css`and `ThemeProvider` from "@emotion/react" package
+import { ThemeProvider } from "@emotion/react";
 // import styled components, theming and animation from "./styles.js" file
+import {
+  CardWrapper,
+  ImageWrapper,
+  TextWrapper,
+  TitleWrapper,
+  DescriptionWrapper,
+  ActionsWrapper,
+  Button,
+  PrimaryButton,
+  SecondaryButton,
+} from "./styles";
 
 const hotels = [
   {
@@ -29,22 +43,50 @@ const hotels = [
 // Apply styling to code within the `App` component's `return` statement using styled components, theming, animation and the `css` prop
 function App() {
   return (
-    <main>
-      <img src={logo} alt="" />
-      <div>
+    <main
+      css={{
+        color: "#03045e",
+        background: "#caf0f8",
+        height: "1200px",
+        fontFamily: "helvetica",
+      }}
+    >
+      <img
+        src={logo}
+        alt="logo"
+        css={css`
+          display: absolute;
+          margin-top: 15px;
+          margin-left: 15px;
+          height: 100px;
+          width: 100px;
+        `}
+      />
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 15px;
+          padding: 20px;
+          @media (max-width: 900px) {
+            display: grid;
+          }
+        `}
+      >
         {hotels.map((hotel) => {
           return (
-            <div key={hotel.id}>
-              <img src={hotel.src} alt={hotel.alt} />
-              <div>
-                <h2>{hotel.title}</h2>
-                <h3>{hotel.description}</h3>
-              </div>
-              <div>
-                <button>Details</button>
-                <button>Book</button>
-              </div>
-            </div>
+            <CardWrapper key={hotel.id}>
+              <ImageWrapper src={hotel.src} alt={hotel.alt} />
+              <TextWrapper>
+                <TitleWrapper>{hotel.title}</TitleWrapper>
+                <DescriptionWrapper>{hotel.description}</DescriptionWrapper>
+              </TextWrapper>
+              <ActionsWrapper>
+                <PrimaryButton>Details</PrimaryButton>
+                <SecondaryButton>Book</SecondaryButton>
+              </ActionsWrapper>
+            </CardWrapper>
           );
         })}
       </div>
--- src/styles.js
+++ src/styles.js
@@ -1,1 +1,68 @@
 // create and export styled components along with the animation and theming
+import styled from "@emotion/styled";
+
+export const CardWrapper = styled.div`
+  width: 250px;
+  height: 325px;
+  background: #fff;
+  border-radius: 15px;
+  padding-bottom: 5px;
+  @media (max-width: 900px) {
+    width: 400px;
+  }
+`;
+
+export const ImageWrapper = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 60%;
+  border-radius: 15px 15px 0 0;
+`;
+
+export const TextWrapper = styled.div`
+  padding: 10px;
+  height: 50px;
+`;
+
+export const TitleWrapper = styled.h2`
+  margin: 0;
+  font-size: 20px;
+`;
+
+export const DescriptionWrapper = styled.h3`
+  margin-top: 5px;
+  font-size: 14px;
+  color: #023e8a;
+`;
+
+export const ActionsWrapper = styled.div`
+  margin-left: 10px;
+  padding: 10px 0;
+  display: flex;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  margin-right: 10px;
+  margin-top: 4px;
+  border: 0;
+  border-radius: 15px;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.08);
+  padding: 10px 0;
+  cursor: pointer;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+
+  &:hover {
+    box-shadow: 0 15px 15px rgba(0, 0, 0, 0.16);
+  }
+`;
+
+export const PrimaryButton = styled(Button)`
+  background-color: #03045e;
+  color: #caf0f8;
+`;
+
+export const SecondaryButton = styled(Button)`
+  background-color: #caf0f8;
+  color: #03045e;
+`;
